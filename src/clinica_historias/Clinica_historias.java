4057399454d2e877416ed5469e02a7fdbd1466c6
Jamesllm <ronaljamesllapapascamontes@gmail.com>
2025-06-19 12:19:19 -0500

    first commit

---
+++ src/clinica_historias/Clinica_historias.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clinica_historias;
+
+/**
+ *
+ * @author Llapapasca Montes
+ */
+public class Clinica_historias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
